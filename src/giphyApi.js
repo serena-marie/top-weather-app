6599eb7698c0f2241b8fcbeae30f33b9c702065d
Serena <serena.gutierrez0@gmail.com>
2024-06-28 10:07:50 -0700

    refactor: Improvements api data sanitization and error handling

--- src/giphyApi.js
+++ src/giphyApi.js
@@ -1,11 +1,22 @@
-async function giphy(q = "celebrate") {
+async function giphy() {
+  const q = "celebrate";
   const key = "dzSxeJtetjuDCGswNddYiwaMhwJfDKIw";
   const baseURL = "https://api.giphy.com/v1/gifs/translate";
 
   try {
     const response = await fetch(`${baseURL}?api_key=${key}&s=${q}`);
+    if (!response.ok) {
+      const errorResponse = await response.json();
+      const statusMessage = `Response was not ok. status: ${response.status}`;
+      const moreInfo = errorResponse
+        ? `\n[Giphy_API]: ${JSON.stringify(errorResponse)}`
+        : "";
+      const message = `${statusMessage}${moreInfo}`;
+      console.error(message);
+      throw new Error(message);
+    }
     const json = await response.json();
-    return json.data.images.original.url;
+    return json?.data?.images?.original?.url;
   } catch (error) {
     return { error: `${error}` };
   }
--- src/weatherApi.js
+++ src/weatherApi.js
@@ -8,42 +8,44 @@ const options = {
 };
 
 async function fetchWeather(location = defaultCity) {
-  const loc = location || defaultCity;
+  const loc = (location || defaultCity).replace(/[^a-zA-Z\s,.]/g, "").trim();
 
   try {
     const response = await fetch(
       `${baseUrl}?key=${apiKey}&q=${loc}&aqi=no`,
       options,
     );
-    return response.json();
+
+    if (!response.ok) {
+      const errorResponse = await response.json();
+      const statusMessage = `Response was not ok. status: ${response.status}`;
+      const moreInfo = errorResponse
+        ? `\n[Weather_API]: ${JSON.stringify(errorResponse)}`
+        : "";
+      const message = `${statusMessage}${moreInfo}`;
+      console.error(message);
+      throw new Error(message);
+    }
+
+    return await response.json();
   } catch (error) {
-    return { error: `${error}` };
+    throw new Error(`Error fetching weather data: ${error.message}`);
   }
 }
 
 async function transformData(data) {
   try {
-    const locationData = data?.location;
-    const currentData = data?.current;
-    let city = "",
-      region = "",
-      condition = "",
-      tempC = "",
-      tempF = "",
-      time = "",
-      isDay = "";
-    if (locationData.name) city = locationData.name;
-    if (locationData.region) region = locationData.region;
-    if (currentData.is_day !== undefined && currentData.is_day !== null) {
-      isDay = currentData.is_day === 1;
-    }
-    if (currentData.condition) condition = currentData.condition;
-    if (currentData.temp_c) tempC = currentData.temp_c;
-    if (currentData.temp_f) tempF = currentData.temp_f;
-    if (currentData.last_updated) {
-      const transformedTime = format(new Date(currentData.last_updated), "p");
-      time = transformedTime;
-    }
+    const locationData = data?.location ?? {};
+    const currentData = data?.current ?? {};
+    const city = locationData.name ?? "";
+    const region = locationData.region ?? "";
+    const isDay = currentData.is_day === 1;
+    const condition = currentData.condition ?? "";
+    const tempC = currentData.temp_c ?? "";
+    const tempF = currentData.temp_f ?? "";
+    const time = currentData.last_updated
+      ? format(new Date(currentData.last_updated), "p")
+      : "";
 
     return {
       city,
